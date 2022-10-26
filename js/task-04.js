6fbe312f29a78b2c10b73d928d56605e85d30aeb
Oksanka15 <dzobaoksana1993@gmail.com>
2022-10-26 15:56:20 +0200

    ups

--- js/task-04.js
+++ js/task-04.js
@@ -2,9 +2,9 @@
 // Додай слухачів кліків до кнопок, всередині яких збільшуй або зменшуй значення лічильника.
 // Оновлюй інтерфейс новим значенням змінної counterValue.
 
-// let counterValue = 0;
-// const btnDecrement = document.querySelector('[data-action = "decrement"]');
-// const btnIncrement = document.querySelector('[data-action = "increment"]');
+let counterValue = 0;
+const btnDecrement = document.querySelector('[data-action = "decrement"]');
+const btnIncrement = document.querySelector('[data-action = "increment"]');
 
 const span = document.querySelector('#value');
 const increment = () =>
